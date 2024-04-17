bc0fb98a601b9e47f5ddf3a48638c11eb915253a
zlzlwn <zlzlwn1018@gmail.com>
2024-04-17 18:03:35 +0900

    homewidget update 0407 18:03
    homewidget update

--- ovary_app/lib/widget/body/homewidget.dart
+++ ovary_app/lib/widget/body/homewidget.dart
@@ -1,25 +1,209 @@
 import 'package:flutter/material.dart';
-
-
-
-
-
-
-
+import 'package:flutter/widgets.dart';
 
 class HomeWidget extends StatelessWidget {
-          @override
-          Widget build(BuildContext context) {
-          // Figma Flutter Generator MainWidget - FRAME
-            return Column(
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          //pcos버튼
+          IconButton(
+            onPressed: () {},
+            icon: ClipRRect(
+              borderRadius:
+                  BorderRadius.circular(5), //---------------왜 사진이 안깍이는지 확인필요
+              child: Image.asset(
+                "images/pcosbutton.png",
+                width: MediaQuery.of(context).size.width / 1.3,
+                height: MediaQuery.of(context).size.height / 4,
+                // 높이 조절
+              ),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 ElevatedButton(
-                  onPressed: () {
-                    
-                  }, 
-                  child: Text("PCOS란?"))
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromRGBO(255, 215, 222, 1),
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    fixedSize: Size( MediaQuery.of(context).size.width / 4,
+                      MediaQuery.of(context).size.height / 6
+                     ),
+                  ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                        child: Text(
+                          'PCOS 란?',
+                          style: TextStyle(
+                              fontSize: 12, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                        child: Image.asset(
+                          "images/hospital.png",
+                          width: 70,
+                          height: 70,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 0, 30, 0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromRGBO(255, 215, 222, 1),
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5)),
+                      fixedSize: Size( MediaQuery.of(context).size.width / 4,
+                      MediaQuery.of(context).size.height / 6
+                     ),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                          child: Text(
+                            'PCOS 란?',
+                            style: TextStyle(
+                                fontSize: 12, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                          child: Image.asset(
+                            "images/hospital.png",
+                            width: 70,
+                            height: 70,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromRGBO(255, 215, 222, 1),
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    fixedSize: Size( MediaQuery.of(context).size.width / 4,
+                      MediaQuery.of(context).size.height / 6
+                     ),
+                  ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                        child: Text(
+                          'PCOS 란?',
+                          style: TextStyle(
+                              fontSize: 12, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                        child: Image.asset(
+                          "images/hospital.png",
+                          width: 70,
+                          height: 70,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ],
-            );
-          }
-        }
-        
+            ),
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromRGBO(255, 215, 222, 1),
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5)),
+                    fixedSize: Size( MediaQuery.of(context).size.width / 4,
+                      MediaQuery.of(context).size.height / 6
+                     ),
+                  ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                        child: Text(
+                          'PCOS 란?',
+                          style: TextStyle(
+                              fontSize: 12, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                        child: Image.asset(
+                          "images/hospital.png",
+                          width: 70,
+                          height: 70,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(30, 0, 0, 0),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color.fromRGBO(255, 215, 222, 1),
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(5)),
+                      fixedSize: Size( MediaQuery.of(context).size.width / 4,
+                      MediaQuery.of(context).size.height / 6
+                     ),
+                    ),
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                          child: Text(
+                            'PCOS 란?',
+                            style: TextStyle(
+                                fontSize: 12, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                          child: Image.asset(
+                            "images/hospital.png",
+                            width: 70,
+                            height: 70,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
