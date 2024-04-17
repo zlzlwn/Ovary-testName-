77ad4aae4048bfad9db1155f72940b1e06bd8c8c
zlzlwn <zlzlwn1018@gmail.com>
2024-04-17 13:55:22 +0900

    폴더 생성 2024-04-17 13:55
    폴더 생성 2024-04-17 13:55

---
+++ ovary_app/lib/widget/body/homewidget.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+
+
+
+
+
+
+
+class HomeWidget extends StatelessWidget {
+          @override
+          Widget build(BuildContext context) {
+          // Figma Flutter Generator MainWidget - FRAME
+            return Column(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    
+                  }, 
+                  child: Text("PCOS란?"))
+              ],
+            );
+          }
+        }
+        
