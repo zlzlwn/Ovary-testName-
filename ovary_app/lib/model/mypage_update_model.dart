a7c6145840d5a5db19ad97f9a10a448e14b2c7e2
zlzlwn <zlzlwn1018@gmail.com>
2024-04-19 16:43:43 +0900

    2024-04-19 16:43유저 업데이트
    2024-04-19 16:43유저 업데이트

---
+++ ovary_app/lib/model/mypage_update_model.dart
@@ -0,0 +1,15 @@
+class MyInfo{
+    String nickname;
+  String email;
+  String password;
+  String imagepath;
+  
+
+  MyInfo({
+    required this.nickname,
+    required this.email,
+    required this.password,
+    required this.imagepath,
+  });
+
+}
