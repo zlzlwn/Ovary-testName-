44d391d2f9d4cb1dffb90e8f4570ebc29e97dc5e
Sumin Kim <411sumin@gmail.com>
2024-04-20 12:46:22 +0900

    2024-04-20
    생리주기 정보 FirebaseStorage 기록 남기게 추가. 생리주기 차트 구현.

--- ovary_app/lib/view/period_calendar.dart
+++ ovary_app/lib/view/period_calendar.dart
@@ -1,15 +1,16 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
+import 'package:ovary_app/view/cycle_history.dart';
 import 'package:ovary_app/view/home.dart';
 import 'package:ovary_app/view/period_Input.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class PeriodCalender extends StatefulWidget {
     const PeriodCalender({super.key});
-
     @override
     State<PeriodCalender> createState() => _PeriodCalenderState();
 }
@@ -22,66 +23,30 @@ class _PeriodCalenderState extends State<PeriodCalender> {
     DateTime? _rangeEnd;
     int periodLength = 7; // Default period length
     int cycleLength = 28; // Default cycle length 
+    DateTime? nextPeriodDay; // predicted period start day 
     
 
-    @override
-    void initState() {
-        super.initState();
-        retrievePeriodData();
-    }
+String formatNextPeriodDay(DateTime? nextPeriodDay) {
+  if (nextPeriodDay != null) {
+    return DateFormat('yyyy-MM-dd').format(nextPeriodDay);
+  } else {
+    return '';
+  }
+}
 
-    retrievePeriodData() async {
+String formatFocusedDay (DateTime? _focusedDay){
+  if(_focusedDay != null){
+    return DateFormat('yyyy-MM-dd').format(_focusedDay);
+  }else{
+    return '';
+  }
+}
 
-    // // Hardcoded email for testing purposes 
-    // String email = 'chosun@naver.com';
 
-    // Get the email from the box
-    final box = GetStorage();
-    String? email = box.read<String>('email');
-
-    if (email == null) {
-        // Email not found in the box, handle appropriately
-        print('Email not found in the box');
-        return;
-    }
-
-    try {
-
-        // Query Firestore to find the document that matches the hardcoded email
-        QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-            .collection('user')
-            .where('email', isEqualTo: email)
-            .limit(1)
-            .get();
-
-        // If a document is found, extract the period data
-        if (querySnapshot.docs.isNotEmpty) {
-            DocumentSnapshot document = querySnapshot.docs.first;
-            List<dynamic> periodData = document['period'] as List<dynamic>;
-
-            // Extract formattedDate, periodLength, and cycleLength from the array
-            String formattedDate = periodData[0] as String;
-            periodLength = periodData[1] as int;
-            cycleLength = periodData[2] as int;
-
-            // Parse formattedDate to DateTime
-            DateTime rangeStart = DateFormat('yyyy-MM-dd').parse(formattedDate);
-            DateTime rangeEnd = rangeStart.add(Duration(days: periodLength - 1));
-
-            // Update the state with the retrieved values
-            setState(() {
-                _rangeStart = rangeStart;
-                _rangeEnd = rangeEnd;
-                _focusedDay = rangeStart;
-            });
-
-            print('Period data retrieved and state updated');
-        } else {
-            print('No document found with the specified email');
-        }
-        } catch (error) {
-            print('Error retrieving data: $error');
-        }
+@override
+void initState() {
+    super.initState();
+    retrievePeriodData();
 }
 
 
@@ -94,7 +59,7 @@ Widget build(BuildContext context) {
             actions: [
                 IconButton(
                     onPressed: () {
-                        Get.to(() => const PeriodInput());
+                      Get.to(() => const PeriodInput());
                     },
                     icon: const Icon(Icons.edit)
                 )
@@ -139,7 +104,7 @@ Widget build(BuildContext context) {
                     setState(() {});
                 }
             },
-            onPageChanged: _onPageChanged,
+            // onPageChanged: _onPageChanged,
           ),
 
           const SizedBox(height: 20),
@@ -152,8 +117,8 @@ Widget build(BuildContext context) {
                   width: 20,
                   height: 20,
                   decoration: BoxDecoration(
-                      color: Colors.pinkAccent.withOpacity(0.8),
-                      shape: BoxShape.circle,
+                  color: Colors.pinkAccent.withOpacity(0.8),
+                  shape: BoxShape.circle,
                   ),
               ),
               const SizedBox(width: 8),
@@ -173,38 +138,60 @@ Widget build(BuildContext context) {
               const Text('오늘날짜', style: TextStyle(fontSize: 16)),
             ],
           ),
+          const SizedBox(height: 40),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                padding:  const EdgeInsets.all(8.0),
+                child: Text('생리 시작날짜:  ${formatFocusedDay(_focusedDay)} ',style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 18)),
+                  ),
+                Padding(
+                padding:  const EdgeInsets.all(8.0),
+                child: Text('생리 기간 :   $periodLength 일',style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 18)),
+                  ),
+                Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('다음 생리 예정일:  ${formatNextPeriodDay(nextPeriodDay)}',style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 18),),
+                ),
+              ],
+            ),
+          
           const SizedBox(height: 30),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          const SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text('생리 시작일:   $_rangeStart', style: const TextStyle(fontSize: 18)),
+            ElevatedButton(
+              onPressed: () => Get.to(const Home()),
+              style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(120, 50),
+                    backgroundColor: Colors.pink[300],
+                  ), 
+              child: Text(
+                '홈으로',
+                style: TextStyle(
+                fontSize: 20,
+                color: Theme.of(context).colorScheme.onPrimary,
+                ),
+              ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text('생리 기간:     $periodLength 일',style: const TextStyle(fontSize: 18)),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(' 생리 주기:    $cycleLength 일',style: const TextStyle(fontSize: 18)),
+            const SizedBox(width: 20),
+            ElevatedButton(
+              onPressed: () => Get.to(const periodCycleChart()),
+              style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(120, 50),
+                    backgroundColor: Colors.pink[300],
+                  ), 
+              child: Text(
+                '생리주기 기록보기',
+                style: TextStyle(
+                fontSize: 20,
+                color: Theme.of(context).colorScheme.onPrimary,
+                ),
+              ),
             ),
           ],
-        ),
-        const SizedBox(height: 95),
-        ElevatedButton(
-          onPressed: () => Get.to(const Home()),
-          style: ElevatedButton.styleFrom(
-                minimumSize: const Size(120, 50),
-                backgroundColor: Colors.pink[300],
-              ), 
-          child: Text(
-            '홈으로',
-            style: TextStyle(
-            fontSize: 20,
-            color: Theme.of(context).colorScheme.onPrimary,
-            ),
-          ),
         ),
       ],
     ),
@@ -214,32 +201,145 @@ Widget build(BuildContext context) {
 
     // ---Functions---
 
-    // Handle page change (Change of Months)
-    void _onPageChanged(DateTime focusedDay) {
-        // Calculate the days difference between the new focused day and the previous focused day
-        int daysDifference = focusedDay.difference(_focusedDay).inDays;
+    // // Handle page change (Change of Months)
+    // void _onPageChanged(DateTime focusedDay) {
+    //   // Calculate the days difference between the new focused day and the previous focused day
+    //   int daysDifference = focusedDay.difference(_focusedDay).inDays;
 
-        // Adjust rangeStart and rangeEnd based on the cycle length and days difference
-        if (_rangeStart != null) {
-            // Calculate the new range start based on the days difference and cycle length
-            _rangeStart = focusedDay.add(Duration(days: daysDifference));
+    //   // Adjust rangeStart and rangeEnd based on the cycle length and days difference
+    //   if (_rangeStart != null) {
+    //       // Calculate the new range start based on the days difference and cycle length
+    //       _rangeStart = focusedDay.add(Duration(days: daysDifference));
 
-            // Calculate the new range end based on the new range start and period length
-            _rangeEnd = _rangeStart!.add(Duration(days: periodLength - 1));
+    //       // Calculate the new range end based on the new range start and period length
+    //       _rangeEnd = _rangeStart!.add(Duration(days: periodLength - 1));
+    //   } else {
+    //       // If there is no range start, initialize it with the new focused day
+    //       _rangeStart = focusedDay;
+    //       _rangeEnd = focusedDay.add(Duration(days: periodLength - 1));
+    //   }
+
+    //   // Update the focused day
+    //   _focusedDay = focusedDay;
+
+    //   // Trigger a state update to refresh the UI
+    //   setState(() {});
+    // }
+
+
+
+  _showNoDataDialogue(){
+    showCupertinoDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return CupertinoAlertDialog(
+          title: const Text("생리주기를 입력해주세요!"),
+          actions: [
+            CupertinoDialogAction(
+              child: const Text("확인"),
+              onPressed: () {
+                // Dismiss the dialog
+                Navigator.of(context).pop();
+                // Go back to the previous screen
+                Get.to(const PeriodInput());
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
+predictPeriodDate(){
+nextPeriodDay = _rangeStart!.add(Duration(days: cycleLength));
+
+print(nextPeriodDay);
+
+setState(() {});
+}
+
+
+retrievePeriodData() async {
+  // Retrieve the email from GetStorage
+  final box = GetStorage();
+  String? email = box.read<String>('email');
+
+  if (email == null) {
+      print('Email not found in the box');
+      return;
+  }
+
+  try {
+      // Query Firestore to find the document with the specified email
+      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+          .collection('user')
+          .where('email', isEqualTo: email)
+          .limit(1)
+          .get();
+
+    if (querySnapshot.docs.isNotEmpty) {
+        // Document found for the specified email
+        DocumentSnapshot document = querySnapshot.docs.first;
+        final data = document.data() as Map<String, dynamic>?;
+
+      // Check if the 'period' map exists
+      if (data != null && data.containsKey('period')) {
+          Map<String, dynamic> periodMap = data['period'] as Map<String, dynamic>;
+
+        // Get all keys from periodMap and sort them in descending order
+        List<String> keys = periodMap.keys.toList();
+        keys.sort((a, b) => b.compareTo(a));
+
+        if (keys.isNotEmpty) {
+            // Get the latest key (first in the sorted list)
+            String latestKey = keys.first;
+
+            // Retrieve the latest period data array using the latest key
+            List<dynamic>? latestPeriodData = periodMap[latestKey];
+
+            if (latestPeriodData != null && latestPeriodData.length >= 3) {
+                // Retrieve formatted date, period length, and cycle length
+                String formattedDate = latestPeriodData[0] as String;
+                periodLength = latestPeriodData[1] as int;
+                cycleLength = latestPeriodData[2] as int;
+
+                // Convert formatted date to DateTime
+                _rangeStart = DateFormat('yyyy-MM-dd').parse(formattedDate);
+                _rangeEnd = _rangeStart!.add(Duration(days: periodLength - 1));
+
+                // Set the focused day to _rangeStart
+                _focusedDay = _rangeStart!;
+
+                //Calculate next period date: 
+                predictPeriodDate();
+                // Refresh the state to reflect the changes
+                setState(() {});
+
+                print('Latest period data retrieved and state updated');
+            } else {
+                // Handle case where period data is invalid or not present
+                _showNoDataDialogue();
+                print('No valid period data found for the latest key');
+            }
         } else {
-            // If there is no range start, initialize it with the new focused day
-            _rangeStart = focusedDay;
-            _rangeEnd = focusedDay.add(Duration(days: periodLength - 1));
+            // Handle case where there are no keys in the 'period' map
+            _showNoDataDialogue();
+            print('No keys found in the period map');
         }
-
-        // Update the focused day
-        _focusedDay = focusedDay;
-
-        // Trigger a state update to refresh the UI
-        setState(() {});
-    }
-
-
-
+      } else {
+          // Handle case where the 'period' map is not present in the document
+          _showNoDataDialogue();
+          print('No period data found in the specified document');
+      }
+  } else {
+      // Handle case where no document is found for the specified email
+      _showNoDataDialogue();
+      print('No document found with the specified email');
+  }
+  } catch (error) {
+      print('Error retrieving data: $error');
+  }
+}
 
 }//END
