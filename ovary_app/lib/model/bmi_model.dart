17fdb236a99694af2ac6e99756ad489a8b9e7a28
shin-na-ra <nas221@naver.com>
2024-04-20 15:52:28 +0900

    2024.04.20 snr : 체중 변화 chart 생성

---
+++ ovary_app/lib/model/bmi_model.dart
@@ -0,0 +1,13 @@
+class BmiModel {
+
+  String date;
+  int weight;
+  int height;
+
+  BmiModel({
+    required this.date,
+    required this.weight,
+    required this.height
+  });
+  
+}
--- ovary_app/lib/widget/body/weight_chart_widget.dart
+++ ovary_app/lib/widget/body/weight_chart_widget.dart
@@ -1,22 +1,93 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:ovary_app/vm/vm_bmiData.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:ovary_app/model/bmi_model.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
-class WeightChartWidget extends StatelessWidget {
-  
-  const WeightChartWidget({super.key});
+class WeightChartWidget extends StatefulWidget {
+  const WeightChartWidget({Key? key}) : super(key: key);
+
+  @override
+  _WeightChartWidgetState createState() => _WeightChartWidgetState();
+}
+
+class _WeightChartWidgetState extends State<WeightChartWidget> {
+  final box = GetStorage();
+  List hData = [];
+  List wData = [];
+  List dData = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
 
   @override
   Widget build(BuildContext context) {
-
-    final VmBmiData vmBmiData = Get.put(VmBmiData());
-    vmBmiData.getData();
-
     return Center(
-      child: vmBmiData.hdata.isEmpty
-      ? const CircularProgressIndicator()
-      : Text('정보 가져옴'),
-      
+      child: dData.isNotEmpty
+          ? SizedBox(
+              width: 380,
+              height: 400,
+              child: SfCartesianChart(
+                legend: const Legend(isVisible: true),
+                tooltipBehavior: TooltipBehavior(enable: true),
+                series: [
+                  LineSeries<BmiModel, dynamic>(
+                    dataSource: List.generate(
+                      dData.length,
+                      (index) => BmiModel(
+                        date: dData[index],
+                        weight: wData[index],
+                        height: hData[index],
+                      ),
+                    ),
+                    color: Colors.red[300],
+                    xValueMapper: (BmiModel model, _) => model.date,
+                    yValueMapper: (BmiModel model, _) => model.weight,
+                    dataLabelSettings:
+                    const DataLabelSettings(isVisible: true),
+                  )
+                ],
+                primaryXAxis: const CategoryAxis(
+                  title: AxisTitle(text: '날짜'),
+                ),
+                primaryYAxis: const NumericAxis(
+                  title: AxisTitle(text: '몸무게'),
+                ),
+              ),
+            )
+          : const CircularProgressIndicator(),
     );
   }
+
+  //firebase에서 데이터 가져오기
+  getData() async {
+    String email = box.read('email');
+
+    QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+        .collection('write')
+        .where('email', isEqualTo: email)
+        .limit(7)
+        .get();
+
+    if (querySnapshot.docs.isNotEmpty) {
+      final List<Map<String, dynamic>> dataList = querySnapshot.docs
+          .map((doc) => {
+                'height': doc['height'],
+                'weight': doc['weight'],
+                'date': doc['insertdate'],
+              })
+          .toList();
+
+      for (int i = 0; i < dataList.length; i++) {
+        wData.add(dataList[i]['weight']);
+        hData.add(dataList[i]['height']);
+        dData.add(dataList[i]['date']);
+      }
+      setState(() {}); // 데이터가 업데이트 되었음을 알려줌
+    }
+  }
 }
+
