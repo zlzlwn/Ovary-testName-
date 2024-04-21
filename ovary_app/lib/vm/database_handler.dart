3ba7e6b3cf44a3452b7f6bcb08d9746b05ac1931
zlzlwn <zlzlwn1018@gmail.com>
2024-04-21 16:12:05 +0900

    login sqlite  db 설정중 commit
    login sqlite  db 설정중 commit

---
+++ ovary_app/lib/vm/database_handler.dart
@@ -0,0 +1,44 @@
+import 'package:ovary_app/model/users.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHandler {
+  Future<Database> initializeDB() async {
+    //db를 어디위치
+    String path = await getDatabasesPath();
+    return openDatabase(
+      join(path, 'user.db'),
+      onCreate: (db, version) async {
+        //테이블 만들기(초기 구동시에만 작동
+        await db.execute(//사진은 blob으로 가져와야 한다!
+            "create table users (id integer primary key autoincrement, email text");
+      },
+      version: 1,
+      
+    );
+  }
+
+//검색
+  Future<List<Users>> queryUsers() async {
+    final Database db = await initializeDB();
+    final List<Map<String, Object?>> queryResults = 
+    await db.rawQuery('select * from users');
+    return queryResults.map((e) => Users.fromMap(e)).toList();
+  }
+
+  //입력
+  // return 받기 싫으면 <void>를 쓰면 된다
+  Future<int> insertUsers(Users user) async {
+    int result = 0;
+    final Database db = await initializeDB();
+    result = await db.rawInsert(
+      'insert into users(email) values (?)',
+      [user.email]
+    );
+    return result;
+  }
+
+  
+}
+
+
