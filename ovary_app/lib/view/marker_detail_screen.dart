9271967cc12b44b18d5b5d8468c4d0ad328413e7
ssujjy <132913985+ssujjy@users.noreply.github.com>
2024-04-22 09:26:11 +0900

    지도 검색과 현재 위치 되게 변경.
    mysql에서 데이터 가져와서 마커하게 수정예정.

---
+++ ovary_app/lib/view/marker_detail_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MarkerDetailScreen extends StatelessWidget {
+  const MarkerDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Marker Detail'),
+      ),
+      body: Center(
+        child: Text('Marker detail screen'),
+      ),
+    );
+  }
+}
--- ovary_app/lib/widget/body/hospitalmap_widget.dart
+++ ovary_app/lib/widget/body/hospitalmap_widget.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
+
+import '../../view/marker_detail_screen.dart';
 // import 'package:flutter_google_places/flutter_google_places.dart' as loc;
 // import 'package:google_api_headers/google_api_headers.dart' as header;
 // import 'package:google_maps_webservice/places.dart' as places;
@@ -20,72 +23,10 @@ class _HospitalMapWidgetState extends State<HospitalMapWidget> {
   double longitude = 0;
   GoogleMapController? _controller;
   final CameraPosition _kGooglePlex = const CameraPosition(
-    target: LatLng(33.298037, 44.2879251),
-    zoom: 10,
+    target: LatLng(37.532600, 127.024612),
+    zoom: 15,
   );
 
-  /* search functions--------------------------------------
-  Future<void> _handleSearch() async {
-    places.Prediction? p = await loc.PlacesAutocomplete.show(
-        context: context,
-        apiKey: 'your map key,
-        onError: onError,// call the onError function below
-        mode: loc.Mode.overlay,
-        language: 'en',//you can set any language for search
-        strictbounds: false,
-        types: [],
-        decoration: InputDecoration(
-            hintText: 'search',
-            focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(20),
-                borderSide: const BorderSide(color: Colors.white))),
-        components: []// you can determine search for just one country
-);
-
-    displayPrediction(p!, homeScaffoldKey.currentState);
-  }
-
-  void onError(places.PlacesAutocompleteResponse response) {
-    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-      elevation: 0,
-      behavior: SnackBarBehavior.floating,
-      backgroundColor: Colors.transparent,
-      content: AwesomeSnackbarContent(
-        title: 'Message',
-        message: response.errorMessage!,
-        contentType: ContentType.failure,
-      ),
-    ));
-  }
-
-  Future<void> displayPrediction(
-      places.Prediction p, ScaffoldState? currentState) async {
-    places.GoogleMapsPlaces _places = places.GoogleMapsPlaces(
-        apiKey: kGoogleApiKey,
-        apiHeaders: await const header.GoogleApiHeaders().getHeaders());
-    places.PlacesDetailsResponse detail =
-        await _places.getDetailsByPlaceId(p.placeId!);
-// detail will get place details that user chose from Prediction search
-    final lat = detail.result.geometry!.location.lat;
-    final lng = detail.result.geometry!.location.lng;
-    _markers.clear(); //clear old marker and set new one
-    final marker = Marker(
-      markerId: const MarkerId('deliveryMarker'),
-      position: LatLng(lat, lng),
-      infoWindow: const InfoWindow(
-        title: '',
-      ),
-    );
-    setState(() {
-      _markers['myLocation'] = marker;
-      _controller?.animateCamera(
-        CameraUpdate.newCameraPosition(
-          CameraPosition(target: LatLng(lat, lng), zoom: 15),
-        ),
-      );
-    });
-  }
-  -------------------------------------------*/
   getCurrentLocation() async {
     bool serviceEnabled;
     PermissionStatus permissionGranted;
@@ -136,61 +77,67 @@ class _HospitalMapWidgetState extends State<HospitalMapWidget> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children :[Container(
-          margin: const EdgeInsets.symmetric(horizontal: 10),
-          width: double.infinity,
-          height: double.infinity,
-          child: GoogleMap(
-            mapType: MapType.normal,
-            myLocationEnabled: true,
-            initialCameraPosition: _kGooglePlex,
-            markers: _markers.values.toSet(),
-            onTap: (LatLng latlng) {
-              latitude = latlng.latitude;
-              longitude = latlng.longitude;
-              final marker = Marker(
-                markerId: const MarkerId('myLocation'),
-                position: LatLng(latitude, longitude),
-                infoWindow: const InfoWindow(
-                  title: 'AppLocalizations.of(context).will_deliver_here',
-                ),
-              );
-              setState(() {
-                _markers['myLocation'] = marker;
-              });
-            },
-            onMapCreated: (GoogleMapController controller) {
-              _controller = controller;
-            },
-          ),
+        children :[
+          GoogleMap(
+          mapType: MapType.normal,
+          myLocationEnabled: true,
+          initialCameraPosition: _kGooglePlex,
+          markers: _markers.values.toSet(),
+          onTap: (LatLng latlng) {
+            latitude = latlng.latitude;
+            longitude = latlng.longitude;
+            final marker = Marker(
+              markerId: const MarkerId('myLocation'),
+              position: LatLng(latitude, longitude),
+              infoWindow: const InfoWindow(
+                title: 'AppLocalizations.of(context).will_deliver_here',
+              ),
+            );
+            setState(() {
+              _markers['myLocation'] = marker;
+            });
+          },
+          onMapCreated: (GoogleMapController controller) {
+            _controller = controller;
+          },
         ),
         Positioned(
-            left: 10,// you can change place of search bar any where on the map
-            child: TextField(
-                        textInputAction: TextInputAction.search,
-                        decoration: InputDecoration(
-                          hintText: "장소, 버스, 지하철, 주소 검색",
-                          border: InputBorder.none,
-                          contentPadding: EdgeInsets.only(left: 15.0, top: 15.0),
-                          suffixIcon: IconButton(
-                            icon: Icon(Icons.search),
-                            onPressed: () {
-                              // searchAndNavigate();
-                            }, 
-                            iconSize: 30.0,
-                          )
+            top: 10,
+            child: CupertinoButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    CupertinoPageRoute(
+                        builder: (context) => MarkerDetailScreen()));
+              },
+              child: Container(
+                width: MediaQuery.of(context).size.width - 21,
+                height: 44,
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.all(Radius.circular(8))),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(left: 16.0),
+                      child: Text(
+                        '위치 검색',
+                        style: const TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.grey,
                         ),
-                        onChanged: (val) {
-                          setState(() {
-                            // searchAddr = val;
-                            // searchCtg = val;
-                          }
-                        );
-                      },
-                      onSubmitted: (term) {
-                        // searchAndNavigate();
-                      },
+                      ),
                     ),
+                    const Spacer(),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 13.0),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ]
       ),
