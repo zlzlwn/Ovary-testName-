720a8cc836524e91e245214edd18752c0c7bc825
shin-na-ra <nas221@naver.com>
2024-04-26 12:56:27 +0900

    2024.04.26 snr : BMI 지표 변경 및 지표 기준 추가

--- ovary_app/lib/vm/bmiQuest.dart
+++ ovary_app/lib/vm/bmiQuest.dart
@@ -13,10 +13,12 @@ class BmiQuestProvider extends ChangeNotifier {
   bmiCalc() {
     bmi = weight / ((height * 0.01) * (height * 0.01));
 
-    if(bmi >= 30){
+    if(bmi >= 35){
       result = '귀하의 bmi지수는 ${bmi.toStringAsFixed(2)}이고 고도비만 입니다.';
+    } else if(bmi >= 30) {
+      result = '귀하의 bmi지수는 ${bmi.toStringAsFixed(2)}이고 2단계 비만 입니다.';
     } else if(bmi >= 25) {
-      result = '귀하의 bmi지수는 ${bmi.toStringAsFixed(2)}이고 비만 입니다.';
+      result = '귀하의 bmi지수는 ${bmi.toStringAsFixed(2)}이고 1단계 비만 입니다.';
     } else if(bmi >= 23) {
       result = '귀하의 bmi지수는 ${bmi.toStringAsFixed(2)}이고 과체중 입니다.';
     } else if(bmi >= 18.5) {
